3d4693abb4f7e4289e04dfcac65bada0fef7e69f
atif1019 <aali@nanthealth.com>
2025-07-22 23:19:49 +0500

    chnages to vuetify

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <Navbar />
+    <v-layout>
+      <Sidebar />
+      <v-main style="min-height: calc(100vh - 64px); min-width: 100vw">
+        <NuxtPage />
+      </v-main>
+    </v-layout>
+  </v-app>
+</template>
+
+<script setup>
+import Sidebar from '~/components/Sidebar.vue'
+import Navbar from '~/components/Navbar.vue'
+</script>
---
+++ pages/form.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card class="pa-6" max-width="1200" >
+    <v-card-title>My Form</v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field label="Name" v-model="form.name" />
+        <v-text-field label="Email" v-model="form.email" />
+        <v-text-field label="Phone" v-model="form.phone" />
+        <v-text-field label="Address" v-model="form.address" />
+        <v-btn color="primary" class="mt-4" @click="submit">Submit</v-btn>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const form = ref({
+  name: '',
+  email: '',
+  phone: '',
+  address: ''
+})
+
+const submit = () => {
+  console.log('Form Submitted:', form.value)
+}
+</script>
